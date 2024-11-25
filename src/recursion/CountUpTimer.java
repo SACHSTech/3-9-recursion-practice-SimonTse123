d85e96d196e6d168933b056bd0ea7bc25d75fd4f
SimonTse123 <simon.tse25@ycdsbk12.ca>
2024-11-25 10:45:15 -0500

    Finished CoutUpTimer

--- src/recursion/CountUpTimer.java
+++ src/recursion/CountUpTimer.java
@@ -2,7 +2,12 @@ package recursion;
 
 public class CountUpTimer {
     public static void countUp(int n) {
-        // solution here
+        if (n == 0) {
+            return;
+        } else {
+            countUp(n - 1); 
+            System.out.print(n + " ");
+        }
     }
 
     public static void main(String[] args) {
